f6df673f113d6a4e23d09af61e9e31e838162509
aram <plm9606@naver.com>
2020-02-07 01:28:01 +0900

    stack: 탑

---
+++ programmers/stack.queue/top.js
@@ -0,0 +1,19 @@
+function solution(heights) {
+    var answer = [];
+    
+    const len = heights.length;
+    
+    while(heights.length >1){
+        let FIND = false
+        let node = heights.pop();
+        for(let i = heights.length-1;i>=0;i--){
+            if(heights[i] > node) {answer.unshift(i+1); 
+                                   FIND = true;
+                                   break;}
+        }
+        if(!FIND)
+        answer.unshift(0);
+    }
+    answer.unshift(0)
+    return answer;
+}
